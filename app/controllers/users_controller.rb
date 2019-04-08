e807ad3fb38f6d2c86f822fa395df45e784359ee
thebigsteph <nzali.stephane86@gmail.com>
2019-04-08 15:45:48 +0100

    add edit action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,19 +5,28 @@ class UsersController < ApplicationController
 
     def create
         @user = User.new(user_params)
-    
-        if @user.save 
+
+        if @user.save
             redirect_to new_user_path
-        else 
-            render :new 
+        else
+            render :new
         end
     end
-    
-    private 
 
-    def user_params 
+    def edit
+      @user = User.find(params[:id])
+    end
+
+    def update
+      @user = User.find(params[:id])
+      @user.update(user_params)
+    end
+
+    private
+
+    def user_params
         params.require(:user).permit(:username, :email, :password)
     end
-    
-    
+
+
 end
